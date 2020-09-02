7457908f7394806325ae9102757544ebf7f1586a
Graham Wallace <grahamjwallace@gmail.com>
2020-09-02 20:36:33 +0100

    Initial connection test

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,14 @@
 const mongodb = require('mongodb') 
 
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connet to the database ' + error) 
+    }
+
+    console.log('Connected') 
+})
