a7dde937fa587ac8736f74b3fb5b1ad25dd9eb4e
Graham Wallace <grahamjwallace@gmail.com>
2020-09-02 20:41:50 +0100

    Insert of a single document to the users collection

--- src/mongodb.js
+++ src/mongodb.js
@@ -2,13 +2,24 @@ const mongodb = require('mongodb')
 
 const MongoClient = mongodb.MongoClient
 
+// Set up these constants
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
+// Set up the connection
 MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
     if(error) {
         return console.log('Unable to connet to the database ' + error) 
     }
 
     console.log('Connected') 
+
+    // Connect to a specific database
+    const db = client.db(databaseName)
+
+    // Insert a single document
+    db.collection('users').insertOne({
+        name: 'Bob',
+        age: 60
+    })
 })
