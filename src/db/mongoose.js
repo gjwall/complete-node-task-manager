9862b1bccf38646eccf1fa1e73e6a79e41d78dae
Graham Wallace <grahamjwallace@gmail.com>
2020-09-04 21:30:34 +0100

    After first validation video

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose') 
+const validator = require('validator')
 
 const databaseName = 'task-manager-api'
 const connectionURL = 'mongodb://127.0.0.1:27017/' + databaseName
@@ -10,43 +11,63 @@ mongoose.connect(connectionURL, {
 })
 
 // User model
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     }, 
-//     age:  {
-//         type: Number
-//     }
-// })
-
-// const me = new User({
-//     name: 'Graham',
-//     age: 43
-// })
-
-// me.save().then((obj) => {
-//     console.log(obj)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-// Task model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid email address')
+            }
+        }
     },
-    completed: {
-        type: Boolean
+    age:  {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
     }
 })
 
-const aTask = new Task({
-    description: 'Task One',
-    completed: false
+const me = new User({
+    name: '   Mia   ',
+    email: '   MIA@mia.COM   '
+    //age: 11
 })
 
-aTask.save().then((savedTask) => {
-    console.log(savedTask)
+me.save().then((obj) => {
+    console.log(obj)
 }).catch((error) => {
     console.log(error)
 })
+
+// Task model
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const aTask = new Task({
+//     description: 'Task One',
+//     completed: false
+// })
+
+// aTask.save().then((savedTask) => {
+//     console.log(savedTask)
+// }).catch((error) => {
+//     console.log(error)
+// })
