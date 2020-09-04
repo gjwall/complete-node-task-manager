62764cfa6ca09b3f1f260e2de12ebac277f925fe
Graham Wallace <grahamjwallace@gmail.com>
2020-09-04 14:58:29 +0100

    Update many test

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,6 +1,4 @@
-const mongodb = require('mongodb') 
-
-const MongoClient = mongodb.MongoClient
+const {MongoClient, ObjectID} = require('mongodb') 
 
 // Set up these constants
 const connectionURL = 'mongodb://127.0.0.1:27017'
@@ -17,28 +15,39 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     // Connect to a specific database
     const db = client.db(databaseName)
 
-    const tasksCollection = 'tasks'
-    const taskArray = [
-        {
-            description: 'task one',
-            completed: true
-        },
-        {
-            description: 'task two',
-            completed: false
-        },
-        {
-            description: 'task three',
+    // const tasksCollection = 'tasks'
+    // const taskArray = [
+    //     {
+    //         description: 'task one',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'task two',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'task three',
+    //         completed: true
+    //     }
+    // ]
+
+    // db.collection(tasksCollection).insertMany(taskArray, (error, result) => {
+    //     if(error) {
+    //         return console.log('The insert many failed ' + error)
+    //     }
+
+    //     console.log(result.ops) 
+    // })
+
+    db.collection('tasks').updateMany({
+    }, {
+        $set: {
             completed: true
         }
-    ]
-
-    db.collection(tasksCollection).insertMany(taskArray, (error, result) => {
-        if(error) {
-            return console.log('The insert many failed ' + error)
-        }
-
-        console.log(result.ops) 
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
     })
 
 
