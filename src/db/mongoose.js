fe920292fd5696d358968dc1bc9ead78e9a55333
Graham Wallace <grahamjwallace@gmail.com>
2020-09-04 19:15:07 +0100

    Initial mongoose and users collection creation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -8,3 +8,26 @@ mongoose.connect(connectionURL, {
     useUnifiedTopology: true,
     useCreateIndex: true
 })
+
+// User model
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    }, 
+    age:  {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'Graham',
+    age: 43
+})
+
+me.save().then((obj) => {
+    console.log(obj)
+}).catch((error) => {
+    console.log(error)
+})
+
+// Task model
