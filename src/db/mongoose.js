cb93030413da76223964b4d27b2b2f962cbf9860
Graham Wallace <grahamjwallace@gmail.com>
2020-09-04 17:31:34 +0100

    Initial version of the mongoose file

---
+++ src/db/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose') 
+
+const databaseName = 'task-manager-api'
+const connectionURL = 'mongodb://127.0.0.1:27017/' + databaseName
+
+mongoose.connect(connectionURL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
