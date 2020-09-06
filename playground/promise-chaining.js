5e0c0a988722e0b247193295f9e5df761b863c4a
Graham Wallace <grahamjwallace@gmail.com>
2020-09-06 08:43:53 +0100

    Finished the promise chaining example

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,3 +1,12 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
+User.findByIdAndUpdate('5f52a0d4df08c0141c1b782e', { age: 1 } ).then((user) => {
+    console.log(user)
+    // Return a new promise to chain into the next .then
+    return User.countDocuments( { age: 1 })
+}).then((result) => {
+    console.log(result)
+}).catch((e) => {
+    console.log(e)
+})
--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,5 +6,6 @@ const connectionURL = 'mongodb://127.0.0.1:27017/' + databaseName
 mongoose.connect(connectionURL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindAndModify: false
 })
