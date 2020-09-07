50bf4bdee87c7e304e79842a6d33bd14e140ad85
Graham Wallace <grahamjwallace@gmail.com>
2020-09-07 19:04:56 +0100

    Addition of users PATCH end point

--- src/index.js
+++ src/index.js
@@ -101,6 +101,34 @@ app.post('/tasks', async (req, res) => {
     }
 })
 
+///////////////////////////
+// PATCH (update) 
+///////////////////////////
+app.patch('/users/:id', async (req, res) => {
+
+    const allowedUpdates = [ 'name', 'email', 'password', 'age' ]
+    const updates = Object.keys(req.body)
+
+    const isValid = updates.every( (update) => allowedUpdates.includes(update) )    
+
+    if(!isValid) {
+        return res.status(400).send( {error: 'Invalid updates'} )
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true} )
+
+        if(!user) {
+            return res.status(404).send() 
+        }
+
+        res.send(user)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+
 app.listen(port, () => {
     console.log('Server is up on port ', port)
 })
