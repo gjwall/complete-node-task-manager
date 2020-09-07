e97974bfca4b36a96744f7ad915b498c35a43d2a
Graham Wallace <grahamjwallace@gmail.com>
2020-09-07 19:16:56 +0100

    Addition of PATCH /tasks/:id

--- src/index.js
+++ src/index.js
@@ -116,7 +116,7 @@ app.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true} )
+        const user = await User.findByIdAndUpdate( req.params.id, req.body, { new: true, runValidators: true} )
 
         if(!user) {
             return res.status(404).send() 
@@ -128,6 +128,32 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.patch('/tasks/:id', async (req, res) => {
+
+    const allowedUpdates = [ 'description', 'completed' ]
+    const updates = Object.keys(req.body)
+
+    const isValid = updates.every( (update) => allowedUpdates.includes(update) )
+
+    if(!isValid) {
+        return res.status(400).send( {error: 'Invalid updates'} )
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate( req.params.id, req.body, { new: true, runValidators: true} )
+
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+///////////////////////////
+// DELETE (deletion)
+///////////////////////////
 
 app.listen(port, () => {
     console.log('Server is up on port ', port)
