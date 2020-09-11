7c77a02807caf7beb92e8900e46640f5801e801b
Graham Wallace <grahamjwallace@gmail.com>
2020-09-11 13:36:16 +0100

    Amend task.js to use the new approach for saving

--- src/routers/user.js
+++ src/routers/user.js
@@ -71,8 +71,6 @@ router.patch('/users/:id', async (req, res) => {
         updates.forEach((update) => user[update] = req.body[update])
         await user.save()
 
-        //const user = await User.findByIdAndUpdate( req.params.id, req.body, {new: true, runValidators: true} )
-
         if(!user) {
             return res.status(404).send() 
         }
