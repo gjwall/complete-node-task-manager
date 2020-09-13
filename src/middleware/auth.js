1825c15f9b03d36d96da5d2f7fc5a1cf771e4687
Graham Wallace <grahamjwallace@gmail.com>
2020-09-13 14:15:24 +0100

    After logging out lesson

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -16,9 +16,10 @@ const auth = async (req, res, next) => {
             throw new Error() 
         }
 
+        req.token = token 
         req.user = user
-
         next()
+        
     } catch(e) {
         res.status(401).send({error: 'Please authenticate'})
     }
