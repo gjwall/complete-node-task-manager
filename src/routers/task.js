0e3c871c4ff70c6df9692107e6cc76ececb5dc31
Graham Wallace <grahamjwallace@gmail.com>
2020-09-15 13:38:29 +0100

    After filtering data

--- src/routers/task.js
+++ src/routers/task.js
@@ -6,13 +6,25 @@ const router = new express.Router()
 ////////////////////////
 // Get (read) end points
 ////////////////////////
+
+// GET /tasks?completed=true
+// GET /tasks?completed=false
 router.get('/tasks', auth, async (req, res) => {
+    const match = {}
+
+    if(req.query.completed) {
+        match.completed = req.query.completed ==='true'
+    }
 
     try {
-        // Two alternatives for doing the below. See the commented line
-        const tasks = await Task.find({owner: req.user._id})
-        //await req.user.populate('tasks').execPopulate()
-        res.send(tasks) 
+        // Two alternatives for doing the below. See the commented line.
+        // Populate allows further filtering whereas Task.find does not in this course
+        //const tasks = await Task.find({owner: req.user._id})
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate()
+        res.send(req.user.tasks) 
     } catch (e) {
         res.status(500).send()
     }
@@ -56,9 +68,7 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
     const allowedUpdates = [ 'description', 'completed' ]
     const updates = Object.keys(req.body)
-
     const isValid = updates.every( (update) => allowedUpdates.includes(update) )
-
     if(!isValid) {
         return res.status(400).send( {error: 'Invalid updates'} )
     }
@@ -81,12 +91,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 ///////////////////////////
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByOneAndDelete( {_id: req.params.id, owner: req.user._id} )
-
+        const task = await Task.findOneAndDelete( {_id: req.params.id, owner: req.user._id} )
         if(!task) {
             return res.status(404).send()
         }
-
         res.send(task)
     } catch (e) {
         res.status(400).send(e) 
